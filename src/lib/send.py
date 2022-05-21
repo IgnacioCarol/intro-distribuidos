79723e6271016eb3b83891e96329d6d91ade0b80
ignacio lugones <ignacio.lugones@wibson.org>
2022-05-21 04:59:03 -0300

    fix border case where client would keep waiting when server finished processing

--- src/lib/send.py
+++ src/lib/send.py
@@ -5,6 +5,7 @@ from typing import List
 
 BUFFER_SIZE = 1024
 SEPARATOR = b'-'
+ENDING_LIMIT = 10  # times to wait for the ack when finishing sending the file
 
 
 def write_message(key: bytes, message: bytes) -> bytes:
@@ -12,19 +13,29 @@ def write_message(key: bytes, message: bytes) -> bytes:
 
 
 def send_file_stop_wait(socket_connected, filename: str, address):
+    eof_counter = 0
     with open(filename, "rb") as f:
         key = uuid.uuid4().bytes
-        data_to_send = f.read(BUFFER_SIZE - len(key) - 1)
+        data_to_send = read_file(f, key)
         while data_to_send:
-            socket_connected.sendto(write_message(key, data_to_send), address)
+            msg = write_message(key, data_to_send)
+            socket_connected.sendto(msg, address)
             try:
                 data, address = socket_connected.recvfrom(BUFFER_SIZE)
             except socket.timeout:
+                if eof_counter > ENDING_LIMIT:
+                    break
+                if len(msg) < BUFFER_SIZE - len(key) - len(SEPARATOR):
+                    eof_counter += 1
                 print("timeouteo")
                 continue
             if data:
                 key = uuid.uuid4().bytes
-                data_to_send = f.read(BUFFER_SIZE - len(key) - 1)
+                data_to_send = read_file(f, key)
+
+
+def read_file(f, key):
+    return f.read(BUFFER_SIZE - len(key) - len(SEPARATOR))
 
 
 def _get_message(message: bytes) -> List[bytes]:
