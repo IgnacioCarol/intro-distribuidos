3f435c77023f13706edb572f5be404819b2865c9
gillescas <gillescas@fi.uba.ar>
2022-05-25 18:29:20 -0300

    Remove unnecesary separator
    Signed-off-by: gillescas <gillescas@fi.uba.ar>

--- src/lib/send.py
+++ src/lib/send.py
@@ -8,12 +8,11 @@ from lib.timer import RepeatingTimer
 from protocol import *
 
 BUFFER_SIZE = 1024
-SEPARATOR = b""
 ENDING_LIMIT = 10  # times to wait for the ack when finishing sending the file
 
 
 def write_message(key: bytes, message: bytes) -> bytes:
-    return key + SEPARATOR + message
+    return key + message
 
 
 def send_file_stop_wait(socket_connected, filename: str, address):
@@ -30,7 +29,7 @@ def send_file_stop_wait(socket_connected, filename: str, address):
             except socket.timeout:
                 if eof_counter > ENDING_LIMIT:
                     break
-                if len(msg) < BUFFER_SIZE - len(key) - len(SEPARATOR):
+                if len(msg) < BUFFER_SIZE - len(key):
                     eof_counter += 1
                 print("timeouteo")
                 continue
@@ -44,7 +43,7 @@ def _send_callback(socket_connected, address, data_to_send, key):
 
 def _send_data(timers: List[RepeatingTimer], socket_connected: socket.socket, address: str, data_to_send: str, last_chunk_sent: int):
     _send_callback(socket_connected, address, data_to_send, last_chunk_sent)
-    timers.append(RepeatingTimer(10.0, _send_callback, [socket_connected, address, data_to_send, last_chunk_sent]))
+    timers.append(RepeatingTimer(TIMEOUT_TIMER, _send_callback, [socket_connected, address, data_to_send, last_chunk_sent]))
     timers[-1].start()
 
 def send_file_select_and_repeat(socket_connected: socket.socket, filename: str, address: str):
@@ -78,7 +77,7 @@ def send_file_select_and_repeat(socket_connected: socket.socket, filename: str, 
                 last_chunk_sent += 1
 
 def read_file(f: TextIOWrapper, key):
-    return f.read(BUFFER_SIZE - len(key) - len(SEPARATOR))
+    return f.read(BUFFER_SIZE - len(key))
 
 def _get_message_stop_wait(message: bytes) -> List[bytes]:
     return [message[:16], message[16:]]
@@ -92,6 +91,6 @@ def receive_file_stop_wait(socket_connected, path: str, address, processed):
                 continue
             processed.add(key)
             f.write(datachunk)
-            if len(datachunk) < BUFFER_SIZE - len(key) - len(SEPARATOR):
+            if len(datachunk) < BUFFER_SIZE - len(key):
                 break
     print(f"finished receiving and storing file at {path}")
