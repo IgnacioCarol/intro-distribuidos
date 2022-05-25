8aea28af44aa4e775566f6d79e288ada6dbbf4bc
anichucai <acai@fi.uba.ar>
2022-05-25 18:09:09 -0300

    Add send_file_select_and_repeat draft

--- src/lib/send.py
+++ src/lib/send.py
@@ -1,8 +1,11 @@
 import socket
 from time import sleep
 import uuid
+import time
 
 from typing import List
+from protocol import *
+from threading import Timer
 
 BUFFER_SIZE = 1024
 SEPARATOR = b""
@@ -34,6 +37,49 @@ def send_file_stop_wait(socket_connected, filename: str, address):
             key = uuid.uuid4().bytes
             data_to_send = read_file(f, key)
 
+
+def _send_callback(socket_connected, address, data_to_send, key):
+    msg = write_message(bytes(key, ENCODING), data_to_send)
+    socket_connected.sendto(msg, address)
+
+
+def send_file_select_and_repeat(socket_connected: socket.socket, filename: str, address):
+
+#   socket_connected.setblocking(False)
+    eof_counter = 0
+
+    with open(filename, "rb") as f:
+
+        key = uuid.uuid4().bytes
+        data_to_send = read_file(f, key)
+
+        timers = []
+        last_chunk_sent = 0
+        latest_ack = 0
+
+        while data_to_send and last_chunk_sent < WINDOW_SIZE:
+            _send_callback(socket_connected, address, data_to_send, last_chunk_sent)
+            timers.append(Timer(10.0, _send_callback, [socket_connected, address, data_to_send, last_chunk_sent]))
+            timers[-1].start()
+            last_chunk_sent += 1
+
+        while data_to_send:
+            try:
+                ack, address = socket_connected.recvfrom(BUFFER_SIZE)
+                for i in range(latest_ack, ack):
+                    timers[i].cancel()
+                    timers.pop(i)
+
+                latest_ack = ack - 1
+            except socket.timeout:
+                print("timeouteo")
+                continue
+
+            for i in range(WINDOW_SIZE - len(timers)):
+                _send_callback(socket_connected, address, data_to_send, last_chunk_sent)
+                timers.append(Timer(10.0, _send_callback, [socket_connected, address, data_to_send, last_chunk_sent]))
+                timers[-1].start()
+                last_chunk_sent += 1
 
 def read_file(f, key):
     return f.read(BUFFER_SIZE - len(key) - len(SEPARATOR))
