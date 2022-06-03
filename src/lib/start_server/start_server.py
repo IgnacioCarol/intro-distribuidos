6137c10b984e0ef2d0c2626f51cb08b6755e8641
gillescas <gillescas@fi.uba.ar>
2022-06-02 21:24:23 -0300

    Reformat start server
    Signed-off-by: gillescas <gillescas@fi.uba.ar>

--- src/lib/start_server/start_server.py
+++ src/lib/start_server/start_server.py
@@ -1,155 +1,15 @@
 import socket
 import sys
 import threading
+import logging
 import lib.archive as arc
 import lib.errors as lib_errors
 import lib.stop_wait as stop_and_wait
 import lib.selective_repeat as selective_repeat
 import lib.protocol as lib_protocol
+from lib.start_server.downloader import _Downloader
+from lib.start_server.uploader import _Uploader
 from typing import List
-from os import path
-import logging
-
-
-class _Uploader:
-    def __init__(self, storage: str, addr, file_name: str, archive):
-        self.storage = storage
-        self.addr = addr
-        self.server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.file = file_name
-        self.server.settimeout(lib_protocol.TIMEOUT_UPLOAD)
-        self.archive = archive
-
-    def _receive(self):
-        raise Exception('not defined')
-
-    def _handle_release(self):
-        try:
-            self.archive.release_ownership(self.addr, self.file)
-            logging.info(f"[start-server][uploader] Release ownership  {self.file}.")
-        except arc.FileNotInArchiveError:
-            logging.info(
-                f"[start-server][uploader] ERROR: File {self.file} not in archive."
-            )
-        except arc.FileNotOwnedError:
-            logging.info(f"[start-server][uploader] ERROR: File {self.file} not owned.")
-
-    def method(self):
-
-        # Set ownership of file
-        try:
-            logging.info(
-                "[start-server][uploader] Set ownership {}->{}.".format(
-                    self.file, self.addr
-                )
-            )
-            if not self.archive.set_ownership(self.addr, self.file, True):
-                self.server.sendto(
-                    bytes(lib_protocol.ERROR_BUSY_FILE, lib_protocol.ENCODING),
-                    self.addr,
-                )
-                logging.info(
-                    "[start-server][uploader] ERROR Ownership busy of file: {}.".format(
-                        self.file
-                    )
-                )
-                return
-        except arc.FileAlreadyOwnedError:
-            self.server.sendto(
-                bytes(lib_protocol.ERROR_ALREADY_SERVED, lib_protocol.ENCODING),
-                self.addr,
-            )
-            logging.info(
-                "[start-server][uploader] ERROR: File {} has already been served.".format(
-                    self.file
-                )
-            )
-            return
-
-        # Sends SYNC-ACK
-        logging.info(
-            "[start-server][uploader] Sends SYC-ACK to client {}.".format(self.addr)
-        )
-        self.server.sendto(
-            bytes(lib_protocol.MSG_CONNECTION_ACK, lib_protocol.ENCODING), self.addr
-        )
-
-        try:
-            self._receive()
-        except socket.timeout:
-            self._handle_release()
-            return
-
-        logging.info(f"file {self.file} written")
-        self._handle_release()
-
-
-class _Downloader:
-    def __init__(self, storage: str, addr, file_name: str, archive):
-        self.storage = storage
-        self.addr = addr
-        self.server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.file = file_name
-        self.server.settimeout(lib_protocol.TIMEOUT_DOWNLOAD)
-        self.archive = archive
-
-    def _send(self):
-        pass
-
-    def _handle_release(self):
-        try:
-            self.archive.release_ownership(self.addr, self.file)
-            logging.info("[server-downloader] Release ownership {}.".format(self.file))
-        except arc.FileNotInArchiveError:
-            logging.info("[server-downloader] ERROR: File {self.file} not in archive.")
-        except arc.FileNotOwnedError:
-            logging.info("[server-downloader] ERROR: File {self.file} not owned.")
-
-    def method(self):
-
-        # Set ownership of file
-        if not path.exists(f"{self.storage}/{self.file}"):
-            self._handle_release()
-            self.server.sendto(
-                bytes(lib_protocol.ERROR_NONEXISTENT_FILE, lib_protocol.ENCODING),
-                self.addr,
-            )
-            return
-        try:
-            logging.info(
-                "[start-server][downloader] Set ownership {}->{}.".format(
-                    self.file, self.addr
-                )
-            )
-            if not self.archive.set_ownership(self.addr, self.file, False):
-                self.server.sendto(
-                    bytes(lib_protocol.ERROR_BUSY_FILE, lib_protocol.ENCODING),
-                    self.addr,
-                )
-                logging.info(
-                    "[start-server][downloader] ERROR Ownership busy of file: {}.".format(
-                        self.file
-                    )
-                )
-                return
-        except arc.FileAlreadyOwnedError:
-            self.server.sendto(
-                bytes(lib_protocol.ERROR_ALREADY_SERVED, lib_protocol.ENCODING),
-                self.addr,
-            )
-            logging.info(
-                "[start-server][downloader] ERROR: File {} has already been served.".format(
-                    self.file
-                )
-            )
-            return
-        self.server.sendto(
-            bytes(lib_protocol.MSG_CONNECTION_ACK, lib_protocol.ENCODING), self.addr
-        )
-        self._send()
-        logging.info(f"File {self.file} sent")
-        self._handle_release()
-
 
 def get_data(data: bytes) -> List[str]:
     data = str(data, lib_protocol.ENCODING).split()
@@ -225,10 +85,6 @@ class Server:
         self.server.close()
         logging.info("[server] Server closed.")
         sys.exit()
-
-    def __join_connections(self):
-        for connection in self.connections:
-            connection.join()
 
 
 class _UploaderStopAndWait(_Uploader):
