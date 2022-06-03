7837f6ce7060d846a41310117495262dd9055057
gillescas <gillescas@fi.uba.ar>
2022-06-02 21:40:18 -0300

    Rollback change on DownloadSelectiveRepeat

--- src/lib/download/download_selective_repeat.py
+++ src/lib/download/download_selective_repeat.py
@@ -1,9 +1,8 @@
-import os
 from lib.download.download import Download
 import lib.selective_repeat as selective_repeat
 
 
 class DownloadSelectiveRepeat(Download):
     def _receive(self, addr):
-        file_path = "{}/{}".format(os.getcwd(), self.filename)
+        file_path = "{}/{}".format(self.path, self.filename)
         return selective_repeat.receive_file(self.client, file_path, addr)
